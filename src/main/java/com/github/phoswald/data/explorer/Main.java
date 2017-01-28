20e1e4fd30a6a0f104e78f35e2e41383ed5a5517
philip <philip.oswald@gmail.com>
2017-01-28 01:41:25 +0100

    Add CSS styling, run.sh

--- src/main/java/com/github/phoswald/data/explorer/Main.java
+++ src/main/java/com/github/phoswald/data/explorer/Main.java
@@ -13,6 +13,7 @@ import styx.data.Pair;
 import styx.data.Reference;
 import styx.data.Store;
 import styx.data.Value;
+import styx.data.exception.ParserException;
 import styx.http.server.Request;
 import styx.http.server.Response;
 import styx.http.server.Server;
@@ -34,8 +35,9 @@ public class Main {
             server.
                 port(serverPort).
                 routes(
-                    route().path("/").toResource("/index.html"),
-                    route().path("/favicon.ico").toResource(),
+                    route().path("/").toResource("index.html"),
+                    route().path("/data-explorer.css").toResource("data-explorer.css"),
+                    route().path("/favicon.ico").toResource("favicon.ico"),
                     route().path("/browse/**").to(this::browse),
                     route().path("/view/**").to(this::view),
                     route().path("/edit/**").to(this::edit),
@@ -52,29 +54,37 @@ public class Main {
             Optional<Value> value = store.read(reference);
             if(!value.isPresent() || !value.get().isComplex()) {
                 res.status(404);
-            } else {
-                res.contentType("text/html");
-                res.write("<!DOCTYPE html>\n");
-                res.write("<html>\n");
-                res.write("  <head><title>Data Explorer: " + escapeHtml(reference) + "</title></head>\n");
-                res.write("  <body>\n");
-                res.write("    <a href='/'>Home</a>\n");
-                res.write("    <a href='" + getPath("/view", reference) +"'>View</a><br>\n");
-                res.write("    <hr>\n");
-                res.write("    <b>" + escapeHtml(reference) +"</b><br>\n");
-                if(reference.parent().isPresent()) {
-                    res.write("    <a href='" + getPath("/browse", reference.parent().get()) +"'>..</a><br>\n");
-                }
-                for(Pair pair : value.get().asComplex().allEntries()) {
-                    if(pair.value().isComplex()) {
-                        res.write("    <a href='" + getPath("/browse", reference.child(pair.key())) +"'>" + escapeHtml(pair.key()) +"</a> : <a href='" + getPath("/view", reference.child(pair.key())) + "'>{ ... }</a><br>\n");
-                    } else {
-                        res.write("    " + escapeHtml(pair.key()) + " : <a href='" + getPath("/view", reference.child(pair.key())) + "'>" + escapeHtml(pair.value()) + "</><br>\n");
-                    }
-                }
-                res.write("  </body>\n");
-                res.write("</html>\n");
+                return;
             }
+            res.contentType("text/html");
+            res.write("<!DOCTYPE html>\n");
+            res.write("<html>\n");
+            res.write("  <head>\n");
+            res.write("    <title>Data Explorer: " + escapeHtml(reference) + "</title>\n");
+            res.write("    <link rel='stylesheet' href='/data-explorer.css'>\n");
+            res.write("  </head>\n");
+            res.write("  <body>\n");
+            res.write("    <p class='nav'><a href='/'>Home</a>\n");
+            res.write("    <a href='" + getPath("/view", reference) +"'>View</a></p>\n");
+            res.write("    <hr>\n");
+            res.write("    <p class='reference'>" + escapeHtml(reference) +"</p>\n");
+            if(reference.parent().isPresent()) {
+                res.write("    <a href='" + getPath("/browse", reference.parent().get()) +"'>..</a><br>\n");
+            }
+            res.write("    <table>\n");
+            res.write("      <tr><th>Key</th><th>Value</th></tr>\n");
+            for(Pair pair : value.get().asComplex().allEntries()) {
+                res.write("      <tr>\n");
+                if(pair.value().isComplex()) {
+                    res.write("        <td><a href='" + getPath("/browse", reference.child(pair.key())) +"'>" + escapeHtml(pair.key()) +"</a></td><td><a href='" + getPath("/view", reference.child(pair.key())) + "'>{ ... }</a></td>\n");
+                } else {
+                    res.write("        <td>" + escapeHtml(pair.key()) + "</td><td><a href='" + getPath("/view", reference.child(pair.key())) + "'>" + escapeHtml(pair.value()) + "</a></td>\n");
+                }
+                res.write("      </tr>\n");
+            }
+            res.write("    </table>\n");
+            res.write("  </body>\n");
+            res.write("</html>\n");
         }
     }
 
@@ -85,22 +95,26 @@ public class Main {
             Optional<Value> value = store.read(reference);
             if(!value.isPresent()) {
                 res.status(404);
-            } else {
-                Reference parentOrSelf = reference.parent().isPresent() ? reference.parent().get() : reference;
-                res.contentType("text/html");
-                res.write("<!DOCTYPE html>\n");
-                res.write("<html>\n");
-                res.write("  <head><title>Data Explorer: " + escapeHtml(reference) + "</title></head>\n");
-                res.write("  <body>\n");
-                res.write("    <a href='" + getPath("/browse", parentOrSelf) +"'>Browse</a>");
-                res.write("    <a href='" + getPath("/edit", reference) +"'>Edit</a>\n");
-                res.write("    <a href='" + getPath("/content", reference) +"'>Download</a><br>\n");
-                res.write("    <hr>\n");
-                res.write("    <b>" + escapeHtml(reference) +"</b><br>\n");
-                res.write("    <pre>\n" + escapeHtml(generate(value.get(), GeneratorOption.INDENT)) +"\n</pre>\n");
-                res.write("  </body>\n");
-                res.write("</html>\n");
+                return;
             }
+            String content = generate(value.get(), GeneratorOption.INDENT);
+            Reference parentOrSelf = reference.parent().isPresent() ? reference.parent().get() : reference;
+            res.contentType("text/html");
+            res.write("<!DOCTYPE html>\n");
+            res.write("<html>\n");
+            res.write("  <head>\n");
+            res.write("    <title>Data Explorer: " + escapeHtml(reference) + "</title>\n");
+            res.write("    <link rel='stylesheet' href='/data-explorer.css'>\n");
+            res.write("  </head>\n");
+            res.write("  <body>\n");
+            res.write("    <p class='nav'><a href='" + getPath("/browse", parentOrSelf) +"'>Browse</a>");
+            res.write("    <a href='" + getPath("/edit", reference) +"'>Edit</a>\n");
+            res.write("    <a href='" + getPath("/content", reference) +"'>Download</a></p>\n");
+            res.write("    <hr>\n");
+            res.write("    <p class='reference'>" + escapeHtml(reference) +"</p>\n");
+            res.write("    <pre>\n" + escapeHtml(content) +"\n</pre>\n");
+            res.write("  </body>\n");
+            res.write("</html>\n");
         }
     }
 
@@ -108,30 +122,45 @@ public class Main {
         Reference reference = getReference(req);
         logger.info("Edit " + reference);
         try(Store store = Store.file(Paths.get(dataStoreUrl))) {
+            String error = null;
+            String content = null;
             if(req.param("content").isPresent()) {
                 logger.info("Writing " + reference);
-                Value content = parse(req.param("content").get());
-                store.write(reference, content);
+                content = req.param("content").get();
+                try {
+                    store.write(reference, parse(content));
+                } catch(ParserException e) {
+                    error = e.getMessage();
+                }
             }
-            Optional<Value> value = store.read(reference);
-            if(!value.isPresent()) {
-                res.status(404);
-            } else {
-                res.contentType("text/html");
-                res.write("<!DOCTYPE html>\n");
-                res.write("<html>\n");
-                res.write("  <head><title>Data Explorer: " + escapeHtml(reference) + "</title></head>\n");
-                res.write("  <body>\n");
-                res.write("    <a href='" + getPath("/view", reference) +"'>View</a><br>\n");
-                res.write("    <hr>\n");
-                res.write("    <b>" + escapeHtml(reference) +"</b><br>\n");
-                res.write("    <form method='post'>\n");
-                res.write("      <input type='submit' value='Store'><br>\n");
-                res.write("      <textarea cols='150' rows='50' name='content'>\n" + escapeHtml(generate(value.get(), GeneratorOption.INDENT)) +"\n</textarea>\n");
-                res.write("    </form>\n");
-                res.write("  </body>\n");
-                res.write("</html>\n");
+            if(error == null) {
+                Optional<Value> value = store.read(reference);
+                if(!value.isPresent()) {
+                    res.status(404);
+                    return;
+                }
+                content = generate(value.get(), GeneratorOption.INDENT);
             }
+            res.contentType("text/html");
+            res.write("<!DOCTYPE html>\n");
+            res.write("<html>\n");
+            res.write("  <head>\n");
+            res.write("    <title>Data Explorer: " + escapeHtml(reference) + "</title>\n");
+            res.write("    <link rel='stylesheet' href='/data-explorer.css'>\n");
+            res.write("  </head>\n");
+            res.write("  <body>\n");
+            res.write("    <p class='nav'><a href='" + getPath("/view", reference) +"'>View</a></p>\n");
+            res.write("    <hr>\n");
+            res.write("    <p class='reference'>" + escapeHtml(reference) +"</p>\n");
+            if(error != null) {
+                res.write("    <p class='error'>Failed to store: " + escapeHtml(error) +"</p>\n");
+            }
+            res.write("    <form method='post'>\n");
+            res.write("      <input type='submit' value='Store'><br>\n");
+            res.write("      <textarea name='content'>\n" + escapeHtml(content) +"\n</textarea>\n");
+            res.write("    </form>\n");
+            res.write("  </body>\n");
+            res.write("</html>\n");
         }
     }
 
@@ -142,15 +171,15 @@ public class Main {
             Optional<Value> value = store.read(reference);
             if(!value.isPresent()) {
                 res.status(404);
-            } else {
-                res.contentType("text/plain");
-                res.write(generate(value.get(), GeneratorOption.INDENT));
+                return;
             }
+            res.contentType("text/plain");
+            res.write(generate(value.get(), GeneratorOption.INDENT));
         }
     }
 
-    private Reference getReference(Request req) {
-        return parse("<" + req.path().substring(req.path().indexOf('/', 1)) + ">").asReference();
+    private Reference getReference(Request req) { // TODO: improve conversion of param("**") to reference
+        return parse("<" + Paths.get("/").resolve(req.param("**").get()).normalize() + ">").asReference();
     }
 
     private String getPath(String prefix, Reference reference) {
